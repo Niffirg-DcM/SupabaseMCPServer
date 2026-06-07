a80fd8554bf2f2e3f630cf98fb1384e488f88a6d
Niffirg-DcM <mcdonald.w.griffin@gmail.com>
2026-06-07 13:06:53 -0700

    initial files from cargo init and game plan

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+//
+//  main.rs
+//  Supabase MCP Server
+//
+//  Created by Griffin McDonald on 2026-06-07.
+//
+
+//this will be a child process for other projects so they can access a given supabase db.
+//will potentially take in api key and endpoint to allow for customizability in its domain
+fn main() {
+    println!("Hello, world!");
+
+    // Must offer tools for supabase
+
+    // must handle reqests to said tools
+
+    // this will have us looping on stdin to find pipelined inputs
+}
