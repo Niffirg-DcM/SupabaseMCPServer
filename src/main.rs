2f0b96ca48f24698c16909beb17390112e34a77a
Niffirg-DcM <mcdonald.w.griffin@gmail.com>
2026-07-01 16:47:30 -0700

    added first tool offering
    recieved well from stdin pipe.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,30 @@
 //
 //  Created by Griffin McDonald on 2026-06-07.
 //
-
+mod offer_tools;
+mod process_tools;
 //this will be a child process for other projects so they can access a given supabase db.
 //will potentially take in api key and endpoint to allow for customizability in its domain
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+    //offer tools -> listen for inputs and handle them/
+    offer_tools::offer();
 
     // Must offer tools for supabase
+    // tools are total entries, last entry, entries from date-date, range of dates.
+    // loop {
+    //     println!("OUTPUT!!!!!!");
+    // }
+
+
+
 
 
     // must handle reqests to said tools
 
-
     // this will have us looping on stdin to find pipelined inputs
-    
+
 }
+
+
--- src/offer_tools.rs
+++ src/offer_tools.rs
@@ -1,5 +1,54 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OutTools {
+    #[serde(rename = "functionDeclarations", alias = "function_declarations")]
+    function_declarations: Vec<OutDeclarations>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OutDeclarations {
+    name: String,
+    description: String,
+    parameters: OutParams,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OutParams {
+    r#type: String,
+    properties: OutProps
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OutProps {
+    location: OutLocation,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct OutLocation {
+    r#type: String,
+}
 
 
-pub fn offer_tools() {
-    
+pub fn offer() {
+    // format json
+    // grab pieces
+    // send
+    let outgoing_tools = vec![ OutTools {
+                function_declarations: vec![ OutDeclarations {
+                    name: String::from("First_Entry"),
+                    description: String::from("gets the time of the very first Entry from the SupabaseDB."),
+                    parameters: OutParams {
+                            r#type: String::from("OBJECT"), 
+                            properties: OutProps { 
+                                location:  OutLocation {
+                                    r#type: String::from("STRING")
+                                } 
+                            }
+                        }
+                    }]
+            }];
+
+        println!("{}",serde_json::to_string(&outgoing_tools).unwrap());
+
 }
